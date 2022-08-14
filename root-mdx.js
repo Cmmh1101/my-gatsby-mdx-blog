0d702ed51b50eb02db02b07b563e04b27d5e6538
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-08-14 01:06:40 -0500

    add: gatsby MDX provider wrap

---
+++ root-mdx.js
@@ -0,0 +1,11 @@
+import './src/css/main.css'
+import React from 'react'
+import { MDXProvider } from '@mdx-js/react'
+
+const components = {
+  //  logic
+}
+
+export const wrapMDX = ({ element }) => {
+  return <MDXProvider components={components}>{element}</MDXProvider>
+}
