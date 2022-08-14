54912b6a882301e8ef9bb39aa5cb782f83a280fd
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-08-14 01:14:54 -0500

    add: gatsby mdx

--- root-mdx.js
+++ root-mdx.js
@@ -4,6 +4,7 @@ import { MDXProvider } from '@mdx-js/react'
 
 const components = {
   //  logic
+  //   h3: () => <span>an you thought you had a heading 3</span>,
 }
 
 export const wrapMDX = ({ element }) => {
