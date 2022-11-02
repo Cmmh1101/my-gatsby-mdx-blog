c23d0b9ff2fb7ef99f4a64d6569f5632ecf5c930
Carla Montano <cm.emprendiendo@gmail.com>
2022-11-01 23:39:23 -0500

    change: hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,6 +4,11 @@ import { StaticImage } from 'gatsby-plugin-image'
 const Hero = ({ showHeroImg }) => {
   return (
     <header className="hero">
+      <div className="header">
+        <h1>Welcome to my blog</h1>
+        <p>Where I share my journey from teaching to coding and beyond</p>
+      </div>
+
       {showHeroImg && (
         <StaticImage
           src="../assets/person2.svg"
